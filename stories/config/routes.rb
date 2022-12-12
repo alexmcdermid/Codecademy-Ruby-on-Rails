2b4f4b4deeca80cb152ca48435c96cb41fa77854
alexmcdermid <alexander.mcdermid@gmail.com>
2022-12-12 12:36:51 -0800

    signup

--- stories/config/routes.rb
+++ stories/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/about' => 'pages#about'
+  get '/signup' => 'signups#new'
+  post '/signup' => 'signups#create'
+  get '/thanks' => 'pages#thanks'
   root 'pages#home'
 end
